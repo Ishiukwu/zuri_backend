6790bf5f3ff2adc10ddd4f429852d072c762d17d
Samuel Chimezie <samuelogoegbu@gmail.com>
2023-09-09 11:45:16 +0100

    true

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rzygc)e4#sy26q44a^vc8hjq7(8h%6^-wh8^30_37yarmyi6wf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
