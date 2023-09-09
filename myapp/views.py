dc526526ce3303330d03eb93f12f9b8a9ca782fc
Samuel Chimezie <samuelogoegbu@gmail.com>
2023-09-09 11:39:31 +0100

    m

---
+++ myapp/views.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+from django.http import JsonResponse
+
+# Create your views here.
+def getData(request):
+    slack_name = request.GET.get('slack_name', 'Maxim_K')
+    track = request.GET.get('track', 'backend')
+
+    current_day = datetime.now().strftime("%A")
+    utc_time = (datetime.utcnow() + timedelta(minutes=2)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+
+    person = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return JsonResponse(person)
+
+
+
+
